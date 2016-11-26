64009c26e745d01944236ba696183c935d5179a8
KJ <kjprice12@gmail.com>
2016-11-26 13:09:03 -0600

    case study 2 - scribbles

---
+++ scribbles1.R
@@ -0,0 +1,11 @@
+## problem 1
+city = read.csv('data/CityTemp.csv')
+str(city)
+head(city)
+length(unique(city$Country))
+
+
+temp = read.csv('data/Temp.csv')
+str(temp)
+dim(city)
+
---
+++ scribbles2.R
@@ -0,0 +1,5 @@
+## problem 2
+
+library(tseries)
+SNPdata = get.hist.quote('gwph', quote='Close')
+summary(SNPdata)
